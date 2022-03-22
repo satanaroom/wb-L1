05b372da6af8659fca7b98cc021a94b46280cbeb
satanaroom <oficialsatana@mail.ru>
2022-03-22 23:42:34 +0300

    task-13 done

--- task-13/task-13.go
+++ task-13/task-13.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	n := 792341293
+	m := -2133962345
+
+	fmt.Printf("numbers before swap: n = %d, m = %d\n", n, m)
+	n = n + m
+	m = n - m
+	n = n - m
+	fmt.Printf("numbers after  swap: n = %d, m = %d\n", n, m)
 }
