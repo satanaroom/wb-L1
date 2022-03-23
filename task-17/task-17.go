558412da86f85752e0b7bcf8cf3e811acf20d003
satanaroom <oficialsatana@mail.ru>
2022-03-23 14:53:03 +0300

    task-17 done

--- task-17/task-17.go
+++ task-17/task-17.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+)
 
+func main() {
+	x := 3
+	nums := [10]int{1, 2, 3, 4, 6, 10, 20, 30, 40, 100}
+	// Функция Search пакета sort осуществляет бинарный поиск в отсортированной структуре данных
+	i := sort.Search(len(nums), func(i int) bool { return nums[i] >= x })
+	if i < len(nums) && nums[i] == x {
+		fmt.Printf("%d is present at nums[%d]", x, i)
+	} else {
+		fmt.Printf("%d is not present in nums, but %d - index where it would be inserted", x, i)
+	}
 }
