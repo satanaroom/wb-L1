11037cc7293a26249feb37a0c3ccf33776b710da
satanaroom <oficialsatana@mail.ru>
2022-03-23 16:26:10 +0300

    task-25 done

--- task-25/task-25.go
+++ task-25/task-25.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+// Функция, которая ожидает время, переданное в аргументе, после чего отправляет
+// текущее время в канал и тут же считывает его
+func MySleep(d time.Duration) {
+	<-time.After(d)
+}
+
+func main() {
+	unitsOfTime := 3
+	fmt.Println("timer started with", unitsOfTime, "seconds")
+	MySleep(time.Duration(unitsOfTime) * time.Second)
+	fmt.Print("timer expired")
 }
