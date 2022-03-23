5b615599c15d12617cf9946b9eef4a4a902c055e
satanaroom <oficialsatana@mail.ru>
2022-03-23 23:03:28 +0300

    task-14 done

--- task-14/task-14.go
+++ task-14/task-14.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+// Функция, которая в рантайме определяет тип переменной
+func GetValueType(t interface{}) {
+	switch t := t.(type) {
+	default:
+		fmt.Printf("unexpected type %T\n", t)
+	case bool:
+		fmt.Printf("%T\n", t)
+	case int:
+		fmt.Printf("%T\n", t)
+	case string:
+		fmt.Printf("%T\n", t)
+	case chan int:
+		fmt.Printf("%T\n", t)
+	}
+}
+
+func main() {
+	var t interface{}
+
+	num := 10
+	str := "hello"
+	flag := true
+	ch := make(chan int, 1)
+
+	t = num
+	GetValueType(t)
+	t = str
+	GetValueType(t)
+	t = flag
+	GetValueType(t)
+	t = ch
+	GetValueType(t)
 }
