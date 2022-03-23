265ca30fcd92d5ede8bb2a675a1f948b5902411b
satanaroom <oficialsatana@mail.ru>
2022-03-23 15:48:03 +0300

    task-26 done

--- task-26/task-26.go
+++ task-26/task-26.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func IsIsogram(word string) bool {
+	// Создание пустой строки для последующей конкатенации символов
+	cache := ""
+	// Создание регистронезависимости
+	word = strings.ToLower(word)
+	for _, val := range word {
+		// Пропускаем тире, можно еще пробелы, для функции, работающей с предложениями
+		if string(val) == "-" {
+			continue
+			// Проверка содержания в строке cache символа из слова
+		} else if strings.Contains(cache, string(val)) {
+			return false
+			// Если проверка не удалась - добавляем в cache символ
+		} else {
+			cache += string(val)
+		}
+	}
+
+	return true
+}
+
+func main() {
+	testWords := []string{"abcd", "abCdefAaf", "aabcd", "raw-daw", "raw-pol"}
+	for _, word := range testWords {
+		if IsIsogram(word) {
+			fmt.Println("word", word, "is isogram")
+		} else {
+			fmt.Println("word", word, "is not isogram")
+		}
+	}
 }
