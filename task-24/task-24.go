268a919604e387eff54848f2e41be30ca16bfb62
satanaroom <oficialsatana@mail.ru>
2022-03-23 16:46:51 +0300

    task-24 done

--- task-24/task-24.go
+++ task-24/task-24.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+// Структура с инкапсулированными полями - координатами точки
+type Point struct {
+	x float64
+	y float64
+}
+
+// Инкапсулированный метод-конструктор инициализации точки на плоскости
+func (p *Point) newPosition(x, y float64) {
+	p.x = x
+	p.y = y
+}
+
+func main() {
+	var curPoint, newPoint Point
+	// Инициализация текущей точки
+	curPoint.newPosition(1.0, 1.0)
+	// Инициализация конечной точки
+	newPoint.newPosition(3.0, 1.0)
+
+	// Расчет шага по оси X
+	stepX := newPoint.x - curPoint.x
+	// Расчет шага по оси Y
+	stepY := newPoint.y - curPoint.y
+
+	// Расчет расстояния по теореме Пифагора
+	dist := math.Sqrt(stepX*stepX + stepY*stepY)
+	fmt.Println("distance:", dist)
 }
