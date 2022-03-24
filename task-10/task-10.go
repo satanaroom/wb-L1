1bc9525d6a5a1c408af054ba105fe6788603ab4c
satanaroom <oficialsatana@mail.ru>
2022-03-24 20:22:34 +0300

    task-10 done

--- task-10/task-10.go
+++ task-10/task-10.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+// Функция, которая создает мапу с ключем-диапозоном и слайсом значений, находящихся в пределах ключа + переданный шаг
+func GetSubsets(temp []float64, step int) map[int][]float64 {
+	// Инициализация структуры, для хранения подмножеств
+	subset := make(map[int][]float64)
+	// Обход переданного слайса значений с вычислением диапозона и созданием подмножеств
+	for i := range temp {
+		stepKey := int(temp[i]/float64(step)) * step
+		fmt.Println(stepKey, temp[i])
+		subset[stepKey] = append(subset[stepKey], temp[i])
+	}
+	return subset
+}
+
+func main() {
+	temp := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	step := 10
+	subsets := GetSubsets(temp, step)
+	fmt.Println(subsets)
 }
