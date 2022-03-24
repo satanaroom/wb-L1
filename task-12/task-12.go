2d07facd67102011b3b6f57ec8d96d09c256bfba
satanaroom <oficialsatana@mail.ru>
2022-03-24 22:38:57 +0300

    task-12 done

--- task-12/task-12.go
+++ task-12/task-12.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+// Функция удаления дубликатов из множества
+func RemoveDuplicates(str []string) []string {
+	// Объявление результируещего множества
+	var unique []string
+	// Создание мапы для определения уникальности значений в результирующем множестве
+	attend := make(map[string]bool)
+	for _, val := range str {
+		// Если в мапе нет элемента, добавляем
+		if !attend[val] {
+			unique = append(unique, val)
+			// Устанавливаем ключ уникальности
+			attend[val] = true
+		}
+	}
+	return unique
+}
+
+func main() {
+	notSequence := []string{"cat", "cat", "dog", "cat", "tree"}
+	sequence := RemoveDuplicates(notSequence)
+	fmt.Println("sequence:", sequence)
 }
